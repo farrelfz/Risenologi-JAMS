ef4f3f67bd8911adb9783ce34f30b0105521423b
farrelfz <muhamad_1302622084@mhs.unj.ac.id>
2026-07-27 10:11:52 +0700

    chore: remove irrelevant shell scripts and boilerplate folders

--- templates/table-template/table.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-// Use shadcn/ui DataTable component
-export function FeatureTable({ data }: { data: any[] }) {
-  return (
-    <div className="rounded-md border">{/* Table implementation with pagination/filtering */}</div>
-  );
-}
